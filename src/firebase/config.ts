a0c90773dce7ceafbe3561f06daaaa46bb3693ca
Gsss johra Mohra <gsssmohra21@gmail.com>
2025-11-01 11:34:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9677679492-b1542",
+  "appId": "1:433020116769:web:8315943cafdc482094a860",
+  "apiKey": "AIzaSyAuguVDIdzU_mFAL55OMUSzfP6W57fyQVM",
+  "authDomain": "studio-9677679492-b1542.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "433020116769"
+};
