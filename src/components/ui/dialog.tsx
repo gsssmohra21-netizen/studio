274a7b7e356e86f3157b505e5050a238a131c6f7
Gsss johra Mohra <gsssmohra21@gmail.com>
2025-11-04 05:29:05 +0000

    Phone mei back button bhot upar chala gaya click nhi ho raha

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -44,7 +44,7 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-md px-3 py-1 bg-background/70 text-lg font-bold text-foreground opacity-80 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground z-10">
+      <DialogPrimitive.Close className="absolute right-4 bottom-4 rounded-md px-3 py-1 bg-background/70 text-lg font-bold text-foreground opacity-80 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground z-10 sm:top-4 sm:bottom-auto">
         Back
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
