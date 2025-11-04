181b3ed1b31f51f86bc5c9ddba82d5f9e51d4737
Gsss johra Mohra <gsssmohra21@gmail.com>
2025-11-04 12:30:02 +0000

    Please fix jo maine pehle product images add kiye the woh tih dikh rahe

--- next.config.ts
+++ next.config.ts
@@ -13,33 +13,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'i.postimg.cc',
-        port: '',
-        pathname: '/**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'postimg.cc',
-        port: '',
-        pathname: '/**',
+        hostname: '**', // Allow any hostname
       },
     ],
   },
